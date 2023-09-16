bb60eef5cf671e5b946784bd24a4f48e99cda0b2
sajadgilga <majidstic@yahoo.com>
2023-09-16 22:05:25 +0330

    Add addNode function

--- contracts/Orchestrator.sol
+++ contracts/Orchestrator.sol
@@ -7,6 +7,7 @@ contract UrlEmitter {
     uint256 public currentIndex;
 
     event UrlEmitted(address indexed sender, string url);
+    event UrlAdded(string url);
 
     constructor(string[] memory initialUrls) {
         require(initialUrls.length > 0, "Initial Nodes array must not be empty");
@@ -21,5 +22,11 @@ contract UrlEmitter {
         emit UrlEmitted(msg.sender, nextUrl);
 
         currentIndex = (currentIndex + 1) % urls.length;
+    }
+    
+    function addNode(string memory newUrl) public {
+        require(bytes(newUrl).length > 0, "URL must not be empty");
+        urls.push(newUrl);
+        emit UrlAdded(newUrl);
     }
 }
